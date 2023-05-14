951c379fca34b110dc7fa8a3e7dfb1e354fb6649
kevin-tooles <kevinvillalba@tooles.co>
2023-05-14 12:40:45 -0500

    implementacion de correos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { queryParams: ['id'] }
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    data: { queryParams: ['_id'] }
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor(private rutaActiva: ActivatedRoute,
+    private apiService: ApiService) {
+  }
+
+  ngOnInit(): void {
+
+    const id = this.rutaActiva.snapshot.queryParamMap.get('id');
+    console.log(id);
+    this.getDatos(id)
+  }
+
+  async callEndpoint(id: string) {
+
+    this.apiService.post(`renovar/${id}`).subscribe(
+      async (res: any) => {
+        console.log(res);
+        Swal.fire({
+          title: 'Renovacion Realizada',
+          confirmButtonText: 'Cerrar',
+
+        }).then((result) => {
+          if (result.isConfirmed) {
+            //cerrar ventana
+          }
+        })
+      },
+      (error: any) => {
+        console.log('error aceptando terminos', error);
+
+      }
+    );
+
+
+
+  }
+
+  async getDatos(id: any) {
+
+    let body = {
+      "idclientes": id
+    }
+
+    await this.apiService.post('getdatos', body).subscribe(
+      async (res: any) => {
+        console.log(res);
+        let name = res.data[0].Nombre;
+        Swal.fire({
+          title: `Hola ${name} bienvenido,Su licencia vencera pronto si quiere hacer nuevamente uso de nuestros servicio seleccione el boton Renovar esperamos que siga contando con nuestros servicios `,
+          showDenyButton: true,
+          showCancelButton: false,
+          confirmButtonText: 'Renovar',
+          confirmButtonColor: "blue",
+          denyButtonText: `No renovar`,
+        }).then((result) => {
+          if (result.isConfirmed) {
+
+            this.callEndpoint(id);
+          } else if (result.isDenied) {
+            Swal.fire('Changes are not saved', '', 'info')
+          }
+        })
+
+      },
+      (error: any) => {
+        console.log('error consultando el info', error);
+
+      }
+    );
+
+
+
+  }
+
+}
+
+
