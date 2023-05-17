242449cef1846a1a09396e18c5d266e0d93ebe5c
kevin-tooles <kevinvillalba@tooles.co>
2023-05-17 13:31:15 -0500

    cambiar .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'http://localhost:3000/',
+  API: 'http://ec2-3-95-174-252.compute-1.amazonaws.com:3000/',
   // API: 'https://gcp-back-master-rcbmb7p2oa-uc.a.run.app/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API: 'http://localhost:3000/',
+  API: 'http://ec2-3-95-174-252.compute-1.amazonaws.com:3000/',
   // API: 'https://gcp-back-master-rcbmb7p2oa-uc.a.run.app/api/',
 };
 
